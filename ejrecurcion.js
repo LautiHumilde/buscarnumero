8e0eb5170c09726ffb93c5f41eed182760525b20
LautiHumilde <132624519+LautiHumilde@users.noreply.github.com>
2023-09-04 19:11:39 -0300

    Update ejrecurcion.js

--- ejrecurcion.js
+++ ejrecurcion.js
@@ -10,37 +10,32 @@ for (var i = 0; i < arreglo.length - 1; i++) {
 	}
 }
 
-console.log(arreglo);
-
-let medio = arreglo.length / 2;
-
-if (medio.toString().includes(".")) {
-	medio += 0.5;
-}
-
-function buscar(arr, numero, i, j) {
+function buscar(arr, num, i, j) {
 	if (i <= j) {
-		if (arr[medio] === numero) {
-			return true;
-		}
-		if (numero > arr[medio]) {
-			return buscar(arr, numero, medio + 1, j);
+		const medio = Math.floor((i + j) / 2);
+
+		if (arr[medio] === num) {
+			return medio;
+		} else if (arr[medio] < num) {
+			return buscar(arr, num, medio + 1, j);
 		} else {
-			return buscar(arr, numero, i, medio - 1);
+			return buscar(arr, num, i, medio - 1);
 		}
 	} else {
 		return -1;
 	}
 }
 
-let numeroBuscado = 8;
-
-const resultado = buscar(arreglo, numeroBuscado, 0, arreglo.length - 1);
+const numero = 4;
+const resultado = buscar(
+	arreglo,
+	numero,
+	0,
+	arreglo.length - 1
+);
 
 if (resultado !== -1) {
-	console.log(
-		`El número ${numeroBuscado} se encuentra en el índice ${resultado}.`
-	);
+	console.log("tu numero esta");
 } else {
-	console.log(`El número ${numeroBuscado} no se encuentra en el arreglo.`);
+	console.log("tu numero no esta");
 }
